2a23856d521d9d3ff2748082a0426c36faf50d2b
Adam Long <adam.jacob.long@gmail.com>
2024-06-25 20:16:00 -0500

    implement left side bar front end

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,19 @@
+import Sidebar from "@/components/sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName: "Admeen", lastName: "Long"};
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar
+          user={loggedIn}
+        />
+
         {children}
     </main>
   );
